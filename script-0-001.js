309fa47b92ad201a360dd97a1176f51555aadabb
rgwl1tv69 <168993357+rgwl1tv69@users.noreply.github.com>
2024-07-10 06:40:37 -0400

    Update script-0-001.js

--- script-0-001.js
+++ script-0-001.js
@@ -1,1 +1,14 @@
-
+let data = [
+  ["AntiTroll", "creator of the AUTTP, gender is unknown (they also are epic)"],
+  ["Novabium", "admin of the AUTTP, gender is female (nova gender reveal 7/9/2024 confirmed)"],
+  ["RK", "former member of the AUTTP, cool guy"],
+  ["valentino", "goober's lover goober's lover goober's lover goober's lover goober's lover goober's lover goober's lover goober's lover goober's lover goober's lover goober's lover goober's lover goober's lover goober's lover goober's lover goober's lover goober's lover goober's lover goober's lover goober's lover goober's lover goober's lover goober's lover goober's lover goober's lover goober's lover goober's lover goober's lover goober's lover goober's lover goober's lover goober's lover goober's lover goober's lover goober's lover goober's lover goober's lover goober's lover goober's lover goober's lover goober's lover goober's lover goober's lover goober's lover goober's lover goober's lover goober's lover goober's lover goober's lover goober's lover goober's lover goober's lover goober's lover goober's lover goober's lover goober's lover goober's lover goober's lover goober's lover goober's lover goober's lover goober's lover goober's lover goober's lover goober's lover goober's lover goober's lover goober's lover goober's lover goober's lover goober's lover goober's lover goober's lover goober's lover goober's lover goober's lover goober's lover goober's lover goober's lover goober's lover goober's lover goober's lover goober's lover goober's lover goober's lover goober's lover goober's lover goober's lover goober's lover goober's lover goober's lover goober's lover (redirects will come soon idk how to implement it)"],
+  ["goober","the inbodiment of goofiness, he is a hazbin fan and a val fan and a sir pentious fan, his username is bobthecooldude420"],
+  ["Ace", "former admin of the AUTTP, call her ace please"],
+  ["Niko Cube", "brainrotted admin stinky goofy ronald mcdonald ahh chicken nugget max design pro"],
+  ["rgwl", "your on his website right now"],
+  ["WaterfallTV","transgender asriel pfp :shocked:"],
+  ["jad", "boblox industries developer"],
+  ["zornikal", "boblox industries idea giver"],
+  ["normal wuye", "boblox industries developer"]
+]
